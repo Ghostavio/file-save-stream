8b0d2dfaf42f99508e74ea517819a0afc97a096f
howardchi <howardchi@fougood.com>
2014-02-15 15:16:20 +0800

    fix test

--- test.js
+++ test.js
@@ -3,17 +3,19 @@ var fs = require('fs');
 var fileSave = require('./');
 
 describe('should save the file test', function() {
-  it('should save to sample folder', function() {
+  it('should save to sample folder', function(cb) {
     fileSave('sample/test', 'hello world', function(result) {
-     should.not.exist(result); 
+      should.not.exist(result); 
+      cb();
     })
   })
 
-  it('should read the data in sample/test', function() {
+  it('should read the data in sample/test', function(cb) {
     fs.readFile(__dirname + '/sample/test', {encoding: 'utf-8'}, function(err, data) {
       if(err)
         throw err; 
       data.should.equal('hello world');
+      cb()
     })
   })
 
